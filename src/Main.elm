b271104841e0a4b2f008d03391f86256a241ee6c
kyrremoe <kyrre.moe@bekk.no>
2018-04-02 22:40:53 +0200

    Big I in description

--- src/Main.elm
+++ src/Main.elm
@@ -39,7 +39,7 @@ view model =
     div []
         [ img [ src "/nothing.svg" ] []
         , h1 [] [ text "Welcome to kymopedia" ]
-        , p [] [ text "Content will arrive here shortly. (Or at least i intend it to, but I have a tendency to start projects that I don't finish, so who knows)" ]
+        , p [] [ text "Content will arrive here shortly. (Or at least I intend it to, but I have a tendency to start projects that I don't finish, so who knows)" ]
         ]
 
 
